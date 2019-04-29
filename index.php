a2c6a1cbb7c1d415b896dfef6822447d0ba5a512
unknown <pswakowski@gmail.com>
2019-04-29 20:09:01 +0200

    added CV

--- index.php
+++ index.php
@@ -25,12 +25,12 @@ $details->add_details();
                 <h1>Nazywam się <span>Przemysław Swakowski</span>.</h1>
             </header>
             <main>
-                <p>Jestem absolwentem Uniwersytetu Technologiczno-Przyrodniczego w Bydgoszczy. Moją pracą dyplomową było stworzenie aplikacji wspomagającej zarządzanie oraz wizualizację zadań w firmie. Jestem otwarty na wszelkie propozycje współpracy. Jeśli chcesz dowiedzieć się więcej, odwiedź mój profil na <a href="https://www.linkedin.com/in/pswakowski/">LinkedIn</a>.</p>
-                <p>Posiadam doświadczenie w pozycjonowaniu stron internetowych, natomiast aktualnie rozwijam się jako programista. Technologie, w których się poruszam to: python, PHP oraz .NET Core. Większość moich prac jest open source i można je znaleźć na portalu <a href="https://github.com/pswakowski">GitHub</a>.</p>
-                <p>Poza programowaniem możesz mnie znaleźć chodzącego po <a href="https://www.instagram.com/explore/tags/slacklife/">slackline</a> oraz na boisku grającego w piłkę nożną dla <i class="fas fa-star"></i> KS Gwiazda Bydgoszcz.</p>
+                <p>Jestem studentem Uniwersytetu Technologiczno-Przyrodniczego w Bydgoszczy. Moją pracą dyplomową było stworzenie aplikacji wspomagającej zarządzanie oraz wizualizację zadań w firmie. Jestem otwarty na wszelkie propozycje współpracy. Jeśli chcesz dowiedzieć się więcej, odwiedź mój profil na <a class="link" href="https://www.linkedin.com/in/pswakowski/">LinkedIn</a>.</p>
+                <p>Posiadam doświadczenie w pozycjonowaniu stron internetowych, natomiast aktualnie rozwijam się jako programista. Technologie, w których się poruszam to: Node.js, React.js oraz PHP. Większość moich prac jest open source i można je znaleźć na portalu <a class="link" href="https://github.com/pswakowski">GitHub</a>.</p>
+                <p>Poza programowaniem możesz mnie znaleźć chodzącego po <a class="link" href="https://www.instagram.com/explore/tags/slacklife/">slackline</a> oraz na boisku grającego w piłkę nożną dla <i class="fas fa-star"></i> KS Gwiazda Bydgoszcz.</p>
             </main>
             <footer>
-                <span>Kontakt: <i class="fas fa-envelope"></i> pswakowski@gmail.com</span>
+                <span>Kontakt: <i class="fas fa-envelope"></i> <a href="mailto:pswakowski@gmail.com">pswakowski@gmail.com</a> | Curriculum vitae możesz zobaczyć <a class="link-underline" href="/pub/CV.pdf" target="_blank">tutaj</a>.</span>
             </footer>
         <?php else : ?>
             <header>
@@ -38,12 +38,12 @@ $details->add_details();
                 <h1>My name is <span>Przemysław Swakowski</span>.</h1>
             </header>
             <main>
-                <p>I am a graduate of the University of Science and Technology in Bydgoszcz. My diploma thesis was to create an application supporting management and visualization of tasks in the company. I am open to any cooperation proposals. If you want to learn more, please visit my <a href="https://www.linkedin.com/in/pswakowski/">LinkedIn</a> profile.</p>
-                <p>I have experience in SEO, but currently I develop as a programmer. Technologies which I use are: python, PHP and .NET Core. Most of my work is open source and publicly available on <a href="https://github.com/pswakowski">GitHub</a>.</p>
-                <p>Outside of programming, you can find me walking on a <a href="https://www.instagram.com/explore/tags/slacklife/">slackline</a> and on the pitch playing football for <i class="fas fa-star"></i> KS Gwiazda Bydgoszcz.</p>
+                <p>I am a graduate of the University of Science and Technology in Bydgoszcz. My diploma thesis was to create an application supporting management and visualization of tasks in the company. I am open to any cooperation proposals. If you want to learn more, please visit my <a class="link" href="https://www.linkedin.com/in/pswakowski/">LinkedIn</a> profile.</p>
+                <p>I have experience in SEO, but currently I develop as a programmer. Technologies which I use are: Node.js, React.js and PHP. Most of my work is open source and publicly available on <a class="link" href="https://github.com/pswakowski">GitHub</a>.</p>
+                <p>Outside of programming, you can find me walking on a <a class="link" href="https://www.instagram.com/explore/tags/slacklife/">slackline</a> and on the pitch playing football for <i class="fas fa-star"></i> KS Gwiazda Bydgoszcz.</p>
             </main>
             <footer>
-                <span>Contact: <i class="fas fa-envelope"></i> pswakowski@gmail.com</span>
+                <span>Contact: <i class="fas fa-envelope"></i> <a href="mailto:pswakowski@gmail.com">pswakowski@gmail.com</a> | Curriculum vitae you can see <a class="link-underline" href="/pub/CV.pdf" target="_blank">here</a>.</span>
             </footer>
         <?php endif; ?>
     </body>
